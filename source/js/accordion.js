f088fe93b7d79b7bd8c7e2e10d65d31144ec2f48
Dmoldd <oldtime_tula@mail.ru>
2023-06-13 20:50:35 +0300

    Сверствал блок faq

---
+++ source/js/accordion.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const accordions = document.querySelectorAll('.faq__question-title');
+  const accordionContents = document.querySelectorAll('.faq__answer');
+
+  accordions.forEach((itemAcc) => {
+    itemAcc.addEventListener('click', (event) => {
+      event.preventDefault();
+      const context = itemAcc.nextElementSibling;
+
+      if(context.style.maxHeight) {
+        context.style.maxHeight = null;
+        itemAcc.classList.remove('is-opened');
+      } else {
+        context.style.maxHeight = context.scrollHeight + 'px';
+        itemAcc.classList.add('is-opened');
+      }
+
+      accordionContents.forEach((itemCon) => {
+        if (itemCon != context) {
+          itemCon.style.maxHeight = null;
+        }
+      });
+
+      accordions.forEach((item) => {
+        if (item != itemAcc) {
+          item.classList.remove('is-opened');
+        }
+      });
+    });
+  });
+});
