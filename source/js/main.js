1c16b9ca45898d6e94b5f7e3fafe69d5b4a3cc9e
Dmoldd <oldtime_tula@mail.ru>
2023-12-05 16:37:26 +0300

    +

--- source/js/main.js
+++ source/js/main.js
@@ -1,26 +1,21 @@
 const changeThemeButton = document.querySelector('.header__change-theme-button');
 const buttonContainer = document.querySelector('.header__change-theme-button-container');
-const whiteThemeName = document.querySelector('.header__theme-name--white');
-const whiteThemeNameStyle = window.getComputedStyle(whiteThemeName);
-const blackThemeName = document.querySelector('.header__theme-name--black');
-const blackThemeNameStyle = window.getComputedStyle(blackThemeName);
+const themeName = document.querySelector('.header__theme-name');
 
 const pageBody = document.querySelector('.page__body');
 
 const changeThemeName = () => {
-  if (whiteThemeNameStyle.display === 'block') {
-    whiteThemeName.style.display = 'none'
-    blackThemeName.style.display = 'block'
-  } else if (blackThemeNameStyle.display === 'block') {
-    whiteThemeName.style.display = 'block'
-    blackThemeName.style.display = 'none'
+  if (themeName.textContent === 'Светлая тема') {
+    themeName.textContent = 'Тёмная тема';
+  } else {
+    themeName.textContent = 'Светлая тема';
   }
 }
 
 const onChangeTheme = () => {
   pageBody.classList.toggle('dark-theme');
   changeThemeName();
-  console.log(whiteThemeNameStyle.display)
+  console.log(themeName.textContent)
 };
 
-changeThemeButton.addEventListener('click', onChangeTheme);
+buttonContainer.addEventListener('click', onChangeTheme);
