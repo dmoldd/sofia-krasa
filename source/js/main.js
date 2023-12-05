25b340511f91d521160b07a193d78d3aa90f1297
dmoldd <oldtime_tula@mail.ru>
2023-12-05 13:17:19 +0300

    добавляет возможность переключения названия темы

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,26 @@
 const changeThemeButton = document.querySelector('.header__change-theme-button');
-const pageBody =  document.querySelector('.page__body');
+const buttonContainer = document.querySelector('.header__change-theme-button-container');
+const whiteThemeName = document.querySelector('.header__theme-name--white');
+const whiteThemeNameStyle = window.getComputedStyle(whiteThemeName);
+const blackThemeName = document.querySelector('.header__theme-name--black');
+const blackThemeNameStyle = window.getComputedStyle(blackThemeName);
+
+const pageBody = document.querySelector('.page__body');
+
+const changeThemeName = () => {
+  if (whiteThemeNameStyle.display === 'block') {
+    whiteThemeName.style.display = 'none'
+    blackThemeName.style.display = 'block'
+  } else if (blackThemeNameStyle.display === 'block') {
+    whiteThemeName.style.display = 'block'
+    blackThemeName.style.display = 'none'
+  }
+}
 
 const onChangeTheme = () => {
-    pageBody.classList.toggle('dark-theme');
-    console.log(1)
+  pageBody.classList.toggle('dark-theme');
+  changeThemeName();
+  console.log(whiteThemeNameStyle.display)
 };
 
 changeThemeButton.addEventListener('click', onChangeTheme);
