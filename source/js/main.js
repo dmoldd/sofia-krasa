0eecab2ec4bb4e082fbbcd10df7bc94d04ee601f
dmoldd <oldtime_tula@mail.ru>
2023-12-08 18:40:34 +0300

    +

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,4 @@
-import { changeThemes } from "./themes/change-theme.js";
+import {changeThemes} from "./themes/change-theme.js";
 import {addBurgerOpener} from "./navigation/add-burger-opener.js";
 import {addAccordion} from "./navigation/add-accordion.js";
 
