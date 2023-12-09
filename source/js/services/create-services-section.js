6add65a973f15f6638454405424fb19445f4660f
dmoldd <oldtime_tula@mail.ru>
2023-12-10 00:23:31 +0300

    +

--- source/js/services/create-services-section.js
+++ source/js/services/create-services-section.js
@@ -1,35 +1,29 @@
 import {ITEMS_OF_SERVICES, PACKS_OF_SERVICES} from "./data.js"
 
-const servicesContainer = document.createElement('div');
+const servicesTemplate = document.querySelector('.template__services');
+const servicesItemsContainer = servicesTemplate.content.querySelector('.services__list');
 const serviceTemplate = document.querySelector('.template__service-item').content.querySelector('.services__price-item');
-const servicePackTemplate = document.querySelector('.template__service-pack').content.querySelector('.services__item--pack');
-const servicePack = servicePackTemplate.cloneNode(true);
+const finalSection = servicesTemplate.cloneNode(true);
+const finalList = finalSection.content.querySelector('.services').querySelector('.container').querySelector('.services__list');
 
 // работает
 const createItem = (packName) => {
   const serviceElement = serviceTemplate.cloneNode(true);
   serviceElement.querySelector('.services__price-title').textContent = ITEMS_OF_SERVICES[packName].name;
   serviceElement.querySelector('.services__price-count').textContent = `${ITEMS_OF_SERVICES[packName].price} / ${ITEMS_OF_SERVICES[packName].unit}` ;
-  servicesContainer.appendChild(serviceElement);
+  finalList.appendChild(serviceElement);
 };
 
 // работает
 const createItems = () => {
   const itemNames = Object.keys(ITEMS_OF_SERVICES);
   itemNames.forEach((item) => createItem(item));
-  console.log(servicesContainer);
 }
 
-const createPackDescriptions = (packName) => {
-
-}
-
-const createPack = () => {
-
-}
 
 const createServiceSection = () => {
   createItems();
+  console.log(finalList);
 }
 
 export {createServiceSection};
