0c816f9f69d35d3cf277d5892ee685118e2e1e82
dmoldd <oldtime_tula@mail.ru>
2023-12-09 23:33:31 +0300

    продолжаю делать модуль отрисовки

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,11 @@
 import {changeThemes} from "./themes/change-theme.js";
 import {addBurgerOpener} from "./navigation/add-burger-opener.js";
 import {addAccordion} from "./navigation/add-accordion.js";
-import {createServiceItem, createServicePack} from "./services/create-services-section.js";
+import {createServiceSection} from "./services/create-services-section.js";
 
 addBurgerOpener();
 changeThemes();
 addAccordion();
-createServiceItem();
-createServicePack();
+createServiceSection();
+
 
--- source/js/services/create-services-section.js
+++ source/js/services/create-services-section.js
@@ -1,26 +1,37 @@
 import {ITEMS_OF_SERVICES, PACKS_OF_SERVICES} from "./data.js"
 
+const servicesContainer = document.createElement('div');
 const serviceTemplate = document.querySelector('.template__service-item').content.querySelector('.services__price-item');
-
 const servicePackTemplate = document.querySelector('.template__service-pack').content.querySelector('.services__item--pack');
+const servicePack = servicePackTemplate.cloneNode(true);
 
-
-
-console.log(serviceTemplate);
-
-const createServiceItem = () => {
+// работает
+const createItem = (packName) => {
   const serviceElement = serviceTemplate.cloneNode(true);
-  serviceElement.querySelector('.services__price-title').textContent = ITEMS_OF_SERVICES.good2.name;
-  serviceElement.querySelector('.services__price-count').textContent = `${ITEMS_OF_SERVICES.good2.price} / ${ITEMS_OF_SERVICES.good2.unit}` ;
+  serviceElement.querySelector('.services__price-title').textContent = ITEMS_OF_SERVICES[packName].name;
+  serviceElement.querySelector('.services__price-count').textContent = `${ITEMS_OF_SERVICES[packName].price} / ${ITEMS_OF_SERVICES[packName].unit}` ;
+  servicesContainer.appendChild(serviceElement);
 };
 
-const createServicePack = () => {
-  const servicePack = servicePackTemplate.cloneNode(true);
-  const servicePackInnerFirst = servicePack.querySelector('.services__item-inner');
-  const servicePackInnerSecond = servicePack.querySelector('.services__item-inner2');
-  servicePackInnerFirst.querySelector('.services__item-title').textContent = PACKS_OF_SERVICES.pack2.name;
+// работает
+const createItems = () => {
+  const itemNames = Object.keys(ITEMS_OF_SERVICES);
+  itemNames.forEach((item) => createItem(item));
+  console.log(servicesContainer);
 }
 
-export {createServiceItem, createServicePack};
+const createPackDescriptions = (packName) => {
+
+}
+
+const createPack = () => {
+
+}
+
+const createServiceSection = () => {
+  createItems();
+}
+
+export {createServiceSection};
 
 
