e89f1a40056d1cf105b4c9ac53cb3540ce4238ea
dmoldd <oldtime_tula@mail.ru>
2023-12-11 00:47:28 +0300

    Продолжаю программировать отрисовку карточек

--- source/js/services/create-services-section.js
+++ source/js/services/create-services-section.js
@@ -1,16 +1,22 @@
 import {ITEMS_OF_SERVICES, PACKS_OF_SERVICES} from "./data.js"
 
-const servicesTemplate = document.querySelector('.template__services');
-const servicesItemsContainer = servicesTemplate.content.querySelector('.services__list');
+
+const servicesContainer = document.querySelector('.services .container')
 const serviceTemplate = document.querySelector('.template__service-item').content.querySelector('.services__price-item');
-const finalSection = servicesTemplate.cloneNode(true);
+const packTemplate = document.querySelector('.template__service-pack').content.querySelector('.services__item--pack');
+const finalSection = document.querySelector('.template__services').cloneNode(true);
 const finalList = finalSection.content.querySelector('.services').querySelector('.container').querySelector('.services__list');
 
 // работает
-const createItem = (packName) => {
+const createItem = (itemName) => {
   const serviceElement = serviceTemplate.cloneNode(true);
-  serviceElement.querySelector('.services__price-title').textContent = ITEMS_OF_SERVICES[packName].name;
-  serviceElement.querySelector('.services__price-count').textContent = `${ITEMS_OF_SERVICES[packName].price} / ${ITEMS_OF_SERVICES[packName].unit}` ;
+  const description = serviceElement.querySelector('span');
+  if (ITEMS_OF_SERVICES[itemName].description !== null) {
+    description.textContent = ITEMS_OF_SERVICES[itemName].description;
+  } else {
+    serviceElement.querySelector('.services__price-title').textContent = ITEMS_OF_SERVICES[itemName].name;
+  }
+  serviceElement.querySelector('.services__price-count').textContent = `${ITEMS_OF_SERVICES[itemName].price} / ${ITEMS_OF_SERVICES[itemName].unit}`;
   finalList.appendChild(serviceElement);
 };
 
@@ -18,12 +24,29 @@ const createItem = (packName) => {
 const createItems = () => {
   const itemNames = Object.keys(ITEMS_OF_SERVICES);
   itemNames.forEach((item) => createItem(item));
+  servicesContainer.appendChild(finalList);
 }
 
+const createPack = (packName) => {
+  const packElement = packTemplate.cloneNode(true);
+  packElement.querySelector('.services__item-title').textContent = PACKS_OF_SERVICES[packName].name;
+
+  for (let i = 0; i < PACKS_OF_SERVICES[packName].inner_text.length; i++) {
+    console.log(i);
+  }
+
+  console.log(packElement.innerHTML)
+}
+
+const createPacks = () => {
+  const packNames = Object.keys(PACKS_OF_SERVICES);
+  // packNames.forEach((item) => createPack(item));
+  createPack(packNames[0]);
+}
 
 const createServiceSection = () => {
   createItems();
-  console.log(finalList);
+  createPacks();
 }
 
 export {createServiceSection};
