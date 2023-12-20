f14cd96bae2ed6dd41c2909f651152499690a0b4
dmoldd <oldtime_tula@mail.ru>
2023-12-21 02:45:11 +0300

    +

--- source/js/advantages/render-modal.js
+++ source/js/advantages/render-modal.js
@@ -3,14 +3,30 @@ import {ADVANTAGES_ITEMS} from "./data.js";
 const seeMoreButtons = document.querySelectorAll('.advantages__see-more');
 const modal = document.querySelector('.advantages-modal');
 const modalCloseButton = document.querySelector('.advantages-modal__close-button');
-const backgroundPage = document.querySelector('.page');
+const modalTitle = document.querySelector('.advantages-modal__title');
+const modalDescription = document.querySelector('.advantages-modal__text');
+const modalImg = document.querySelector('.advantages-modal__image');
 
-const showModal = () => {
+const showModal = (dataId) => {
+  const itemName = `item${dataId}`;
+  const imgUrlCount = parseInt(dataId) + 1;
+
+  document.body.style.overflow = 'hidden';
   modal.classList.remove('visually-hidden');
+  modalTitle.textContent = ADVANTAGES_ITEMS[itemName].title;
+  modalDescription.textContent = ADVANTAGES_ITEMS[itemName].description;
+  modalImg.src = `./img/slider/${imgUrlCount}.jpg`;
+  
+  if (dataId === '1') {
+    modalDescription.style.fontSize = '14px';
+  } else {
+    modalDescription.style.fontSize = '15px';
+  }
 }
 
 const hideModal = () => {
   modal.classList.add('visually-hidden');
+  document.body.style.overflow = '';
 }
 
 const onModalCloseButtonClick = () => {
@@ -19,16 +35,16 @@ const onModalCloseButtonClick = () => {
 
 const onSeeMoreButtonClick = (evt) => {
   evt.preventDefault();
-  showModal();
+  showModal(evt.target.getAttribute('data-id'));
   modalCloseButton.addEventListener('click', onModalCloseButtonClick);
 }
 
 const renderModal = () => {
   for (let i = 0; i < seeMoreButtons.length; i ++) {
     let button = seeMoreButtons[i];
-    button.addEventListener('click', onSeeMoreButtonClick)
+    button.addEventListener('click', onSeeMoreButtonClick);
+    button.dataset.id = i;
   }
 }
-
 
 export {renderModal};
